dacce655b384849b42b4939046534172b69af9d8
Hitesh Kumar Saini <hit7sh@gmail.com>
2023-07-20 17:12:05 +0530

    Update App.jsx

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Button from '@mui/material/Button';
+import AddTodo from './AddTodo';
+import ShowAllTodos from './showAllTodos';
+import AppBar from './AppBar';
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  // fetch all todos from server
+  const [showAll, setShowAll] = useState(0);
+  const [add, setAdd] = useState(1);
 
   return (
     <>
       <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
+        <AppBar></AppBar>
+        <br />
+        <center>
+          <Button variant="outlined" onClick={()=>{add ? setAdd(0):setAdd(1); }} 
+                  style={{marginLeft:"15px",marginRight:"10px"}}>{!add?"Add a todo":"Hide"}</Button>
+        </center>
+        {!add? "" : <AddTodo />}
+        <br />
+        <br />
+
+        <Button variant="outlined" onClick={()=>{showAll ? setShowAll(0):setShowAll(1); }} 
+                style={{marginLeft:"15px",marginRight:"10px"}}>{showAll?"Hide all todos...":"Show All Todos..."}</Button>
+
+        { showAll === 1 && <ShowAllTodos /> }
       </div>
     </>
   )
 }
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
-}
+
 
 export default App
